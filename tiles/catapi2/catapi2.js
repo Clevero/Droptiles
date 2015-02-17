db7cccddebd31f51bb5c66f1937647236a5b8317
Clevero <m.caspar@live.de>
2015-02-17 01:09:35 +0100

    place catapi2 js code in seperate file

---
+++ tiles/catapi2/catapi2.js
@@ -0,0 +1,7 @@
+//exchange old cat image with a newer one from the api
+function changeCatImage() {
+        var body = document.getElementsByTagName('body')[0];
+	//getTime() is important the we make every time, the functions ist execute, a individual request. otherwise the "new" image loads from the cache
+        var nextUrl = "http://thecatapi.com/api/images/get?format=src&" + new Date().getTime();
+        body.style.backgroundImage = 'url(' + nextUrl + ')';
+}//end changecatImage()
