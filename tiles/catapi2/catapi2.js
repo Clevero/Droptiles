7b536f6ce72908994f26cede2843f812a364827a
Clevero <m.caspar@live.de>
2015-02-18 10:08:13 +0100

    adjust catapi2 image url

--- tiles/catapi2/catapi2.js
+++ tiles/catapi2/catapi2.js
@@ -2,6 +2,6 @@
 function changeCatImage() {
         var body = document.getElementsByTagName('body')[0];
 	//getTime() is important the we make every time, the functions ist execute, a individual request. otherwise the "new" image loads from the cache
-        var nextUrl = "http://thecatapi.com/api/images/get?format=src&" + new Date().getTime();
+        var nextUrl = "http://thecatapi.com/api/images/get?format=src&category=caturday&size=small&" + new Date().getTime();
         body.style.backgroundImage = 'url(' + nextUrl + ')';
 }//end changecatImage()
